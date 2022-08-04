a1487e63cb90dfa0a536abac26859d9c6cb56978
Brendan McShane <brendanmcs7@gmail.com>
2022-08-04 12:25:47 -0400

    add basics

---
+++ CCSODiscordBot/Modules/UserManagement/Greeting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Discord.WebSocket;
+
+namespace CCSODiscordBot.Modules.Greeter
+{
+    public class Greeting
+    {
+        public static async Task UserJoin(SocketGuildUser user)
+        {
+            // Start welcome.
+
+        }
+    }
+}
+
---
+++ CCSODiscordBot/Modules/UserManagement/Leaving.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Discord.WebSocket;
+
+namespace CCSODiscordBot.Modules.UserManagement
+{
+    public class Leaving
+    {
+        public static async Task UserLeft(SocketGuild guild, SocketUser user)
+        {
+            // Notify on leave
+        }
+    }
+}
+
