96150db4abcd833ed3e038fee1fc3419c6c6f3c6
Brendan McShane <brendanmcs7@gmail.com>
2022-08-08 19:32:40 -0400

    Add User CRUD operations

--- CCSODiscordBot/Services/Database/Repository/UserRepository.cs
+++ CCSODiscordBot/Services/Database/Repository/UserRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CCSODiscordBot.Services.DataTables;
 using MongoDB.Driver;
 
 namespace CCSODiscordBot.Services.Database.Repository
@@ -11,6 +12,40 @@ namespace CCSODiscordBot.Services.Database.Repository
         {
             _userCollection = mongoDatabase.GetCollection<DataTables.User>("user");
         }
+
+        // CRUD Operations:
+        #region Create
+        public async Task CreateNewGuildAsync(DataTables.User newGuild)
+        {
+            await _userCollection.InsertOneAsync(newGuild);
+        }
+        #endregion Create
+        #region Read
+        public async Task<List<DataTables.User>> GetAllAsync()
+        {
+            return await _userCollection.Find(_ => true).ToListAsync();
+        }
+        public async Task<DataTables.User> GetByIdAsync(string id)
+        {
+            return await _userCollection.Find(_ => _.Id == id).FirstOrDefaultAsync();
+        }
+        public async Task<List<DataTables.User>> GetByLinqAsync(FilterDefinition<DataTables.User> filter, FindOptions? options = null)
+        {
+            return await _userCollection.Find(filter, options).ToListAsync();
+        }
+        #endregion Read
+        #region Update
+        public async Task UpdategGuildAsync(DataTables.User userToUpdate)
+        {
+            await _userCollection.ReplaceOneAsync(x => x.Id == userToUpdate.Id, userToUpdate);
+        }
+        #endregion Update
+        #region Delete
+        public async Task DeleteGuildAsync(string id)
+        {
+            await _userCollection.DeleteOneAsync(x => x.Id == id);
+        }
+        #endregion Delete
     }
 }
 
