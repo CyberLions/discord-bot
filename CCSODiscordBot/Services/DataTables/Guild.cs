6ee380a8d93d53b5309977331bee2878912674a7
Brendan McShane <brendanmcs7@gmail.com>
2022-08-08 18:41:46 -0400

    Add guild table

---
+++ CCSODiscordBot/Services/DataTables/Guild.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CCSODiscordBot.Services.DataTables
+{
+    public class guild
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("discordId")]
+        public string DiscordID { get; set; }
+
+        [BsonElement("welcomeEnabled")]
+        public bool WelcomeEnabled { get; set; }
+
+        [BsonElement("welcomeChannel")]
+        public ulong WelcomeChannel { get; set; }
+
+        [BsonElement("leaveEnabled")]
+        public bool LeaveEnabled { get; set; }
+    }
+}
+
