87a3016316e06579456842a98f58379e4a285235
Brendan McShane <brendanmcs7@gmail.com>
2022-08-08 18:08:38 -0400

    Add DB

--- CCSODiscordBot/Services/ConfigHandlingService.cs
+++ CCSODiscordBot/Services/ConfigHandlingService.cs
@@ -11,6 +11,10 @@ namespace CCSODiscordBot
         /// The discord token
         /// </summary>
         private string? _DiscordToken;
+        /// <summary>
+        /// The mongo DB connection string
+        /// </summary>
+        private string? _MongoDBConnectionString;
 
         /// <summary>
         /// Initialize the secrets:
@@ -21,8 +25,25 @@ namespace CCSODiscordBot
             // Load the secrets:
             var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
 
-            // Get and set the secrets:
-            _DiscordToken = config["DiscordToken"];
+            // Get and set the DiscordToken:
+            if (config["DiscordToken"] == null)
+            {
+                _DiscordToken = Environment.GetEnvironmentVariable("DiscordToken");
+            }
+            else
+            {
+                _DiscordToken = config["DiscordToken"];
+            }
+
+            // Get and set the MongoDBConnectionString:
+            if (config["MongoDBConnectionString"] == null)
+            {
+                _MongoDBConnectionString = Environment.GetEnvironmentVariable("MongoDBConnectionString");
+            }
+            else
+            {
+                _MongoDBConnectionString = config["MongoDBConnectionString"];
+            }
         }
 
         /// <summary>
@@ -37,6 +58,21 @@ namespace CCSODiscordBot
                     throw new NullReferenceException("The discord token is not set.");
                 }
                 return _DiscordToken;
+            }
+        }
+        /// <summary>
+        /// The MongoDB connection string
+        /// </summary>
+        /// <exception cref="NullReferenceException">Thrown if the connection string is not set</exception>
+        public string MongoDBConnectionString
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_MongoDBConnectionString))
+                {
+                    throw new NullReferenceException("The MongoDB connection string is not set.");
+                }
+                return _MongoDBConnectionString;
             }
         }
     }
