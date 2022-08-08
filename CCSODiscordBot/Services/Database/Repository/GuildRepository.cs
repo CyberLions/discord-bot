b660383922c5fb23367bb075baec87fbb32abdeb
Brendan McShane <brendanmcs7@gmail.com>
2022-08-08 19:29:14 -0400

    Guild CRUD operations

--- CCSODiscordBot/Services/Database/Repository/GuildRepository.cs
+++ CCSODiscordBot/Services/Database/Repository/GuildRepository.cs
@@ -1,16 +1,52 @@
 ﻿using System;
+using System.Threading.Tasks;
+using CCSODiscordBot.Services.DataTables;
 using MongoDB.Driver;
 
 namespace CCSODiscordBot.Services.Database.Repository
 {
     public class GuildRepository:IGuildRepository
     {
-        private readonly IMongoCollection<DataTables.guild> _guildCollection;
+        private readonly IMongoCollection<guild> _guildCollection;
 
         public GuildRepository(IMongoDatabase mongoDatabase)
         {
-            _guildCollection = mongoDatabase.GetCollection<DataTables.guild>("guild");
+            _guildCollection = mongoDatabase.GetCollection<guild>("guild");
         }
+
+        // CRUD Operations:
+        #region Create
+        public async Task CreateNewGuildAsync(guild newGuild)
+        {
+            await _guildCollection.InsertOneAsync(newGuild);
+        }
+        #endregion Create
+        #region Read
+        public async Task<List<guild>> GetAllAsync()
+        {
+            return await _guildCollection.Find(_ => true).ToListAsync();
+        }
+        public async Task<guild> GetByIdAsync(string id)
+        {
+            return await _guildCollection.Find(_ => _.Id == id).FirstOrDefaultAsync();
+        }
+        public async Task<List<guild>> GetByLinqAsync(FilterDefinition<guild> filter, FindOptions? options = null)
+        {
+            return await _guildCollection.Find(filter, options).ToListAsync();
+        }
+        #endregion Read
+        #region Update
+        public async Task UpdategGuildAsync(guild guildToUpdate)
+        {
+            await _guildCollection.ReplaceOneAsync(x => x.Id == guildToUpdate.Id, guildToUpdate);
+        }
+        #endregion Update
+        #region Delete
+        public async Task DeleteGuildAsync(string id)
+        {
+            await _guildCollection.DeleteOneAsync(x => x.Id == id);
+        }
+        #endregion Delete
     }
 }
 
