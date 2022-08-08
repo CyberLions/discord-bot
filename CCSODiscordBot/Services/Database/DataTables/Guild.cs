dad38f1fc21a0e2a8364388a499a1a9d010226b1
Brendan McShane <brendanmcs7@gmail.com>
2022-08-08 18:47:15 -0400

    Update datatable

---
+++ CCSODiscordBot/Services/Database/DataTables/Guild.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CCSODiscordBot.Services.DataTables
+{
+    public class guild
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("discordId")]
+        public ulong DiscordID { get; set; }
+
+        [BsonElement("welcomeEnabled")]
+        public bool WelcomeEnabled { get; set; }
+
+        [BsonElement("welcomeChannel")]
+        public ulong WelcomeChannel { get; set; }
+
+        [BsonElement("welcomeRoles")]
+        public List<ulong>? WelcomeRoles { get; set; }
+
+        [BsonElement("leaveEnabled")]
+        public bool LeaveEnabled { get; set; }
+    }
+}
+
