d0031bfd4310aa8144176044c1558a3febf5881f
Brendan McShane <brendanmcs7@gmail.com>
2022-08-08 18:22:24 -0400

    add tables

---
+++ CCSODiscordBot/Services/DataTables/Users.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CCSODiscordBot.Services.DataTables
+{
+    public class Users
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("discordId")]
+        public string DiscordID { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; }
+
+        [BsonElement("verified")]
+        public bool verified { get; set; }
+
+        [BsonElement("verificationNumber")]
+        public int VerificationNumber { get; set; }
+    }
+}
+
