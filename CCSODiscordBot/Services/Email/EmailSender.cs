073609b05b4aee34f4fe660967fbc492ed7f2916
Brendan McShane <brendanmcs7@gmail.com>
2022-08-09 19:24:55 -0400

    Finish email class

--- CCSODiscordBot/Services/Email/EmailSender.cs
+++ CCSODiscordBot/Services/Email/EmailSender.cs
@@ -12,7 +12,7 @@ namespace CCSODiscordBot.Services.Email
             _configHandlingService = config;
         }
 
-        public async Task SendVerifyCode(int code, string email)
+        public void SendVerifyCode(int code, string recipient)
         {
             var smtpClient = new SmtpClient(_configHandlingService.SMTPAddr)
             {
@@ -20,6 +20,8 @@ namespace CCSODiscordBot.Services.Email
                 Credentials = new NetworkCredential(_configHandlingService.SMTPEmail, _configHandlingService.SMTPPassword),
                 EnableSsl = true
             };
+
+            smtpClient.Send(_configHandlingService.SMTPEmail, recipient, "CCSO Bot Verification Code", "Hi!\nYour verification code is: " + code);
         }
     }
 }
