914a780c7f0e36dde3b491816052d4ed0f6162f3
Brendan McShane <brendanmcs7@gmail.com>
2022-08-13 13:58:20 -0400

    Update embeds

--- CCSODiscordBot/Modules/UserManagement/RolePrompt/RolePromptEmbeds.cs
+++ CCSODiscordBot/Modules/UserManagement/RolePrompt/RolePromptEmbeds.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using Discord;
 
-namespace CCSODiscordBot.Modules.UserManagement.Standing
+namespace CCSODiscordBot.Modules.UserManagement.RolePrompt
 {
-    public class StandingEmbeds
+    public class RolePromptEmbeds
     {
-        public static EmbedBuilder StandingEmbed(bool psuEmail)
+        public static EmbedBuilder Embeds(bool psuEmail, string title = "Class Standing", string? prependBody = null)
         {
             EmbedBuilder embed = new EmbedBuilder();
-            embed.WithTitle("Class Standing");
+            embed.WithTitle(title);
             embed.WithColor(Color.Teal);
-            embed.WithDescription("Select your class/year.");
+
+            string description = "Select your class/year.";
+            if (!string.IsNullOrEmpty(prependBody))
+            {
+                description = prependBody + "\n" + description;
+            }
+            embed.WithDescription(description);
+
             if (!psuEmail)
             {
                 embed.AddField("Student/Faculty", "You did not enter and verify a PSU email, some roles may be disabled.");
