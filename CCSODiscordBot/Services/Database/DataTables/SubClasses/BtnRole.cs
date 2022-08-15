79dea0db6dc04d7a75270074d27e95845ebe6616
Brendan McShane <brendanmcs7@gmail.com>
2022-08-14 23:03:15 -0400

    Add defaults

--- CCSODiscordBot/Services/Database/DataTables/SubClasses/BtnRole.cs
+++ CCSODiscordBot/Services/Database/DataTables/SubClasses/BtnRole.cs
@@ -5,7 +5,17 @@ namespace CCSODiscordBot.Services.Database.DataTables.SubClasses
 {
     public class BtnRole
     {
+        public BtnRole()
+        {
+            Name = "";
+            Description = "";
+            Role = 0;
+            RequireVerification = false;
+            Emote = null;
+        }
+
         public string Name { get; set; }
+        public string Description { get; set; }
         public ulong Role { get; set; }
         public bool RequireVerification { get; set; }
         public IEmote? Emote { get; set; }
