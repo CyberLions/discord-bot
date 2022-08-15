992850611689850a62497960ead6a65b988dd775
Brendan McShane <brendanmcs7@gmail.com>
2022-08-15 18:13:27 -0400

    Use Usings

--- CCSODiscordBot/Services/Email/EmailSender.cs
+++ CCSODiscordBot/Services/Email/EmailSender.cs
@@ -15,26 +15,25 @@ namespace CCSODiscordBot.Services.Email
         public void SendVerifyCode(int code, string recipient, string guild, string username)
         {
             // Build the SMTP client:
-            var smtpClient = new SmtpClient(_configHandlingService.SMTPAddr)
+            using (var smtpClient = new SmtpClient(_configHandlingService.SMTPAddr)
             {
-                Port = (int) _configHandlingService.SMTPPort,
+                Port = (int)_configHandlingService.SMTPPort,
                 Credentials = new NetworkCredential(_configHandlingService.SMTPEmail, _configHandlingService.SMTPPassword),
                 EnableSsl = true
-            };
+            })
+            // Using the SMTP client, execute the following. Then dispose of the object from RAM.
+            { 
 
-            // Send email
-            smtpClient.Send(_configHandlingService.SMTPEmail, recipient, "CCSO Bot Verification Code",
-                "Hi!\n" +
-                "Your verification code is: " + code + " for the " + guild + " discord server. Use the /verify command in Discord with the code to validate you account.\n" +
-                "This code was requested by " + username
-            );
-
+                // Send email
+                smtpClient.Send(_configHandlingService.SMTPEmail, recipient, "CCSO Bot Verification Code",
+                    "Hi!\n" +
+                    "Your verification code is: " + code + " for the " + guild + " discord server. Use the /verify command in Discord with the code to validate you account.\n" +
+                    "This code was requested by " + username
+                );
+            }
 
             // Log this event:
             Console.WriteLine("Email sent to " + recipient + " requested by " + username);
-
-            // Clear the SMTP client from RAM:
-            smtpClient.Dispose();
         }
     }
 }
