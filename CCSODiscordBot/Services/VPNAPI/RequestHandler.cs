e02fbb2a21903f25183db5d71e64aab7837a4174
Brendan McShane <brendanmcs7@gmail.com>
2022-09-25 16:46:18 -0400

    Fix disposable

--- CCSODiscordBot/Services/VPNAPI/RequestHandler.cs
+++ CCSODiscordBot/Services/VPNAPI/RequestHandler.cs
@@ -10,23 +10,26 @@ namespace CCSODiscordBot.Services.VPNAPI
 		public static async Task<bool> MakeVPNRequest(Guild guild, User user)
 		{
 			// Check for null values:
-			if(guild.VPNAPIKey == null || guild.VPNAPIURL == null || user.FirstName == null || user.LastName == null || user.Email == null || user.Verified != true)
+			if (guild.VPNAPIKey == null || guild.VPNAPIURL == null || user.FirstName == null || user.LastName == null || user.Email == null || user.Verified != true)
 			{
 				return false;
 			}
 
 			RequestData data = new RequestData(user.DiscordID.ToString(), user.FirstName, user.LastName, user.Email);
-			HttpClient client = new HttpClient();
-			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-			client.DefaultRequestHeaders.Add("St2-Api-Key", guild.VPNAPIKey);
-			HttpContent content = new StringContent(data.ToString(), System.Text.Encoding.UTF8, "application/json");
-			HttpResponseMessage result = await client.PostAsync(new Uri(guild.VPNAPIURL), content);
-
-			if (result.IsSuccessStatusCode)
+			using (HttpClient client = new HttpClient())
 			{
-				return true;
+
+				client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+				client.DefaultRequestHeaders.Add("St2-Api-Key", guild.VPNAPIKey);
+				HttpContent content = new StringContent(data.ToString(), System.Text.Encoding.UTF8, "application/json");
+				HttpResponseMessage result = await client.PostAsync(new Uri(guild.VPNAPIURL), content);
+
+				if (result.IsSuccessStatusCode)
+				{
+					return true;
+				}
+				return false;
 			}
-			return false;
         }
 	}
 }
