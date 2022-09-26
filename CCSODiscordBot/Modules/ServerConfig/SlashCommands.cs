ea9863b42c60b6535a9ad37dc17e2a04614aa8e4
Brendan McShane <brendanmcs7@gmail.com>
2022-09-25 21:36:47 -0400

    Add remove standing module

--- CCSODiscordBot/Modules/ServerConfig/SlashCommands.cs
+++ CCSODiscordBot/Modules/ServerConfig/SlashCommands.cs
@@ -170,6 +170,39 @@ namespace CCSODiscordBot.Modules.ServerConfig
             await Context.Interaction.FollowupAsync("Settings updated!");
         }
 
+        [SlashCommand("removestanding", "Remove a class standing.")]
+        [EnabledInDm(false)]
+        [DefaultMemberPermissions(GuildPermission.Administrator)]
+        public async Task RemoveStanding(SocketRole role)
+        {
+            await Context.Interaction.DeferAsync(true);
+
+            Guild guild = await _iGuildRepository.GetByDiscordIdAsync(Context.Guild.Id);
+            // Check for new server:
+            if (guild == null)
+            {
+                // Create new
+                guild = await CreateNewGuild(Context.Guild);
+            }
+            // null check
+            if (guild.ClassStandings == null)
+            {
+                guild.ClassStandings = new List<BtnRole>();
+            }
+            List<BtnRole> targets = guild.ClassStandings.FindAll(_ => _.Role == role.Id);
+            if (targets.Count() < 1)
+            {
+                await Context.Interaction.FollowupAsync("No standings found for that role.");
+                return;
+            }
+            foreach(BtnRole target in targets)
+            {
+                guild.ClassStandings.Remove(target);
+            }
+            await _iGuildRepository.UpdateGuildAsync(guild);
+            await Context.Interaction.FollowupAsync("Deleted "+ targets.Count()+" roles.");
+        }
+    
         [SlashCommand("addinterest", "Add a interest role")]
         [EnabledInDm(false)]
         [DefaultMemberPermissions(GuildPermission.Administrator)]
