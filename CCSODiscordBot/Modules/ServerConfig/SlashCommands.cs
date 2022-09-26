0ace576a6733f2667ac52ac0a0588e5f8a7da91c
Brendan McShane <brendanmcs7@gmail.com>
2022-09-25 21:53:02 -0400

    Add remove standing back in

--- CCSODiscordBot/Modules/ServerConfig/SlashCommands.cs
+++ CCSODiscordBot/Modules/ServerConfig/SlashCommands.cs
@@ -170,6 +170,39 @@ namespace CCSODiscordBot.Modules.ServerConfig
             await Context.Interaction.FollowupAsync("Settings updated!");
         }
 
+        [SlashCommand("removestanding", "Remove a class standing.")]
+        [EnabledInDm(false)]
+        [DefaultMemberPermissions(GuildPermission.Administrator)]
+        public async Task RemoveStanding(SocketRole role)
+        {
+            await Context.Interaction.DeferAsync(true);
+
+            Guild guild = await _iGuildRepository.GetByDiscordIdAsync(Context.Guild.Id);
+            // Check for new server:
+            if (guild == null)
+            {
+                // Create new
+                guild = await CreateNewGuild(Context.Guild);
+            }
+            // null check
+            if (guild.ClassStandings == null)
+            {
+                guild.ClassStandings = new List<BtnRole>();
+            }
+            List<BtnRole> targets = guild.ClassStandings.FindAll(_ => _.Role == role.Id);
+            if (targets.Count() < 1)
+            {
+                await Context.Interaction.FollowupAsync("No standings found for that role.");
+                return;
+            }
+            foreach (BtnRole target in targets)
+            {
+                guild.ClassStandings.Remove(target);
+            }
+            await _iGuildRepository.UpdateGuildAsync(guild);
+            await Context.Interaction.FollowupAsync("Deleted " + targets.Count() + " roles.");
+        }
+
         [SlashCommand("addinterest", "Add a interest role")]
         [EnabledInDm(false)]
         [DefaultMemberPermissions(GuildPermission.Administrator)]
@@ -225,6 +258,39 @@ namespace CCSODiscordBot.Modules.ServerConfig
 
             // Notify user:
             await Context.Interaction.FollowupAsync("Settings updated!");
+        }
+
+        [SlashCommand("removeinterest", "Remove a interest role")]
+        [EnabledInDm(false)]
+        [DefaultMemberPermissions(GuildPermission.Administrator)]
+        public async Task RemoveInterest(SocketRole role)
+        {
+            await Context.Interaction.DeferAsync(true);
+
+            Guild guild = await _iGuildRepository.GetByDiscordIdAsync(Context.Guild.Id);
+            // Check for new server:
+            if (guild == null)
+            {
+                // Create new
+                guild = await CreateNewGuild(Context.Guild);
+            }
+            // Check null:
+            if (guild.InterestRoles == null)
+            {
+                guild.InterestRoles = new List<BtnRole>();
+            }
+            List<BtnRole> targets = guild.InterestRoles.FindAll(_ => _.Role == role.Id);
+            if (targets.Count() < 1)
+            {
+                await Context.Interaction.FollowupAsync("No interest found for that role.");
+                return;
+            }
+            foreach (BtnRole target in targets)
+            {
+                guild.InterestRoles.Remove(target);
+            }
+            await _iGuildRepository.UpdateGuildAsync(guild);
+            await Context.Interaction.FollowupAsync("Deleted " + targets.Count() + " roles.");
         }
 
         [SlashCommand("setmemberrole", "Sets the role granted to verified members.")]
