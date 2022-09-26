0d5c72d5236bd64a7498db28f3ef8bbeacfe62dd
Brendan McShane <brendanmcs7@gmail.com>
2022-09-25 21:47:42 -0400

    Add remove interest command

--- CCSODiscordBot/Modules/ServerConfig/SlashCommands.cs
+++ CCSODiscordBot/Modules/ServerConfig/SlashCommands.cs
@@ -226,6 +226,40 @@ namespace CCSODiscordBot.Modules.ServerConfig
             // Notify user:
             await Context.Interaction.FollowupAsync("Settings updated!");
         }
+
+        [SlashCommand("removeinterest", "Remove a interest role")]
+        [EnabledInDm(false)]
+        [DefaultMemberPermissions(GuildPermission.Administrator)]
+        public async Task RemoveInterest(SocketRole role)
+        {
+            await Context.Interaction.DeferAsync(true);
+
+            Guild guild = await _iGuildRepository.GetByDiscordIdAsync(Context.Guild.Id);
+            // Check for new server:
+            if (guild == null)
+            {
+                // Create new
+                guild = await CreateNewGuild(Context.Guild);
+            }
+            // Check null:
+            if (guild.InterestRoles == null)
+            {
+                guild.InterestRoles = new List<BtnRole>();
+            }
+            List<BtnRole> targets = guild.InterestRoles.FindAll(_ => _.Role == role.Id);
+            if (targets.Count() < 1)
+            {
+                await Context.Interaction.FollowupAsync("No interest found for that role.");
+                return;
+            }
+            foreach (BtnRole target in targets)
+            {
+                guild.InterestRoles.Remove(target);
+            }
+            await _iGuildRepository.UpdateGuildAsync(guild);
+            await Context.Interaction.FollowupAsync("Deleted " + targets.Count() + " roles.");
+        }
+
         [SlashCommand("setmemberrole", "Sets the role granted to verified members.")]
         [EnabledInDm(false)]
         [DefaultMemberPermissions(GuildPermission.Administrator)]
